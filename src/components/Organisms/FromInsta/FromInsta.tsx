a70a11f4948ddd0f8a4017172f51e0a7e2a0356f
Darek Mazur <dev@darekmazur.it>
2024-03-07 19:38:37 +0100

    Style heading

--- src/components/Organisms/FromInsta/FromInsta.tsx
+++ src/components/Organisms/FromInsta/FromInsta.tsx
@@ -1,10 +1,11 @@
 import Image from '../../Atoms/Image/Image.tsx';
 import Icon from '../../Atoms/Icon/Icon.tsx';
+import Heading from '../../Atoms/Heading/Heading.tsx';
 
 const FromInsta = () => {
   return(
     <section>
-      Instagram section
+      <Heading tag={'h3'}>Instagram section</Heading>
       <Image />
       Lorem <Icon type={'fas'} icon={'fa-arrow-up-right-from-square'} />
     </section>
