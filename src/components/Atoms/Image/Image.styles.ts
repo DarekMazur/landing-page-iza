b8986b3343d34cc32b624cac09355b834f24099a
Darek Mazur <dev@darekmazur.it>
2024-03-07 19:12:58 +0100

    Style image

---
+++ src/components/Atoms/Image/Image.styles.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const StyledImage = styled.img`
+    width: 15.5rem;
+    height: 15.5rem;
+    object-fit: cover;
+`
--- src/components/Atoms/Image/Image.tsx
+++ src/components/Atoms/Image/Image.tsx
@@ -1,8 +1,9 @@
 import { faker } from '@faker-js/faker';
+import { StyledImage } from './Image.styles.ts';
 
 const Image = () => {
   return(
-    <img src={faker.image.urlLoremFlickr()} alt="" />
+    <StyledImage src={faker.image.urlLoremFlickr()} alt="" />
   )
 }
 
