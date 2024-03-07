a3b7c3b7217178539756df06072f73a0e38718e3
Darek Mazur <dev@darekmazur.it>
2024-03-07 18:23:41 +0100

    Create copyright sketch

--- src/components/Organisms/Footer/Footer.tsx
+++ src/components/Organisms/Footer/Footer.tsx
@@ -1,9 +1,11 @@
 import FooterInfo from '../../Molecules/FooterInfo/FooterInfo.tsx';
+import FooterCopyright from '../../Atoms/FooterCopyright/FooterCopyright.tsx';
 
 const Footer = () => {
   return(
     <footer>
       <FooterInfo />
+      <FooterCopyright />
     </footer>
   )
 }
