d9c7e4120d8defaa7c1009e7c56a22a5ea4a1f2a
Darek Mazur <dev@darekmazur.it>
2024-03-14 15:52:34 +0100

    Fetch last instagram image

--- src/components/Organisms/FromInsta/FromInsta.tsx
+++ src/components/Organisms/FromInsta/FromInsta.tsx
@@ -2,12 +2,35 @@ import Image from '../../Atoms/Image/Image.tsx';
 import Icon from '../../Atoms/Icon/Icon.tsx';
 import Heading from '../../Atoms/Heading/Heading.tsx';
 import { StyledFromInsta } from './FromInsta.styles.ts';
+import { useEffect, useState } from 'react';
 
 const FromInsta = () => {
+  const [data, setData] = useState<string | never>();
+
+  useEffect(() => {
+    async function fetchData() {
+      await fetch(
+        `https://graph.instagram.com/${import.meta.env.VITE_IG_USER}/media?fields=${import.meta.env.VITE_IG_FIELDS}&access_token=${import.meta.env.VITE_IG_KEY}`
+      )
+        .then((res) => {
+          if (res.ok) {
+            return res.json();
+          }
+          throw new Error('Something went wrong');
+        })
+        .then((result) => {
+          setData(result.data[0].media_url);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+    fetchData();
+  }, []);
   return(
     <StyledFromInsta>
       <Heading tag={'h3'}>Ostatnio na Instagramie</Heading>
-      <Image />
+      {data ? <Image url={data} /> : null}
       <a href={`https://instagram.com/${import.meta.env.VITE_IG_URL}`} target={'_blank'}>Zobacz więcej <Icon type={'fas'} icon={'fa-arrow-up-right-from-square'} size={1.3} padding={'0.5rem 0'} /></a>
     </StyledFromInsta>
   )
